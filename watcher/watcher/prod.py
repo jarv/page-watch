436321ac0d896029d2c58c5a314ce2a711308c84
John Jarvis <john@jarv.org>
2014-08-10 16:16:35 -0400

    updating prod settings

--- watcher/watcher/prod.py
+++ watcher/watcher/prod.py
@@ -3,5 +3,5 @@ from .settings import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ['*']
-STATIC_ROOT = '/home/jarv/file-watcher/static-collected'
-STATIC_URL = '/file-watcher-static/'
+STATIC_ROOT = '/home/jarv/file-watcher/static/static'
+STATIC_URL = '/static/'
