25b4f92bdf7ddeb2d6b50777b12e83be4ac782b1
John Jarvis <john@jarv.org>
2014-08-10 15:50:15 -0400

    changing dir name

--- watcher/watcher/prod.py
+++ watcher/watcher/prod.py
@@ -3,5 +3,5 @@ from .settings import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ['*']
-STATIC_ROOT = '/home/jarv/file-watcher/file-watcher-static'
+STATIC_ROOT = '/home/jarv/file-watcher/static-collected
 STATIC_URL = '/file-watcher-static/'
