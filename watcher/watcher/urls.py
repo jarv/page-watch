e21d7687a98ce3399bc45337f9855470ca93d7eb
John Jarvis <john@jarv.org>
2014-08-11 12:09:11 -0400

    removing call to the notify view

--- watcher/watcher/urls.py
+++ watcher/watcher/urls.py
@@ -1,16 +1,15 @@
 from django.conf.urls import patterns, include, url
 from rest_framework.urlpatterns import format_suffix_patterns
-from django.views.decorators.csrf import csrf_exempt
 from watcher import views
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^g/?$', views.CheckGithubUrl.as_view()),
-    url(r'^e/?$', views.Notify.as_view()),
     url(r'^f(?P<gh_path>.*).xml$', views.GetGithubChanges()),
 )
 
